eb87fbd0f33d7a93541cfa944b628290b4408490
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 13:57:47 -0300

    feat: pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../services/apiSports';
+import Title from '../components/Title';
+import Button from '../components/Button';
+import SpanErrors from '../components/SpanErrors';
+// import InputLogin from '../components/InputLogin';
+
 function Login() {
+  const [token, setToken] = useState('');
+  const [errors, setErrors] = useState();
+  const navigate = useNavigate();
+
+  const saveTokenOnLocalStorage = (tokenToLocalStorage) => {
+    localStorage.setItem('token', JSON.stringify(tokenToLocalStorage));
+  };
+
+  const login = async () => {
+    const responseAuth = await auth(token);
+
+    if (Object.keys(responseAuth.data?.errors).length > 0 || responseAuth.error) {
+      setErrors(responseAuth.data.errors);
+      return false;
+    }
+
+    saveTokenOnLocalStorage(token);
+    navigate({ pathname: '/datafilter' });
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <Title text="Login" />
+
+      <input
+        placeholder="Digite sua key"
+        type="text"
+        value={ token }
+        onChange={ ({ target }) => setToken(target.value) }
+      />
+
+      <Button text="Login" onClick={ login } />
+
+      <SpanErrors text={ errors } />
+
+    </div>
   );
 }
 
