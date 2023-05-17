82f9d1f8ffc889b2839c273b730ecc040fd8db77
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 15:20:28 -0300

    feat: página dataFilter agora mostra um select com os países disponíveis

--- src/pages/DataFilter.jsx
+++ src/pages/DataFilter.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
 import SelectorArea from '../components/SelectorArea';
 import Title from '../components/Title';
+import { reqApiSports } from '../services/apiSports';
+import getTokenInLocalStorage from '../utils/localStorage';
 
 function DataFilter() {
+  const [countries, setCountries] = useState([]);
+
+  const COUNTRY_ENDPOINT = 'countries';
+
+  const requestCountries = async () => {
+    const token = getTokenInLocalStorage();
+    const dataCountries = await reqApiSports(COUNTRY_ENDPOINT, JSON.parse(token));
+    if (dataCountries.data?.results) {
+      setCountries(dataCountries.data.response);
+    }
+    // setMyBooks(dataBooks);
+  };
+
+  useEffect(() => {
+    requestCountries();
+  }, []);
+
   return (
     <div>
       <Title text="Tela de filtros" />
-      <SelectorArea text="Escolha um país" />
+      {
+        countries.length > 0 ? (
+          <SelectorArea dataArray={ countries } text="Escolha um país" />
+        ) : (
+          null
+        )
+      }
     </div>
   );
 }
