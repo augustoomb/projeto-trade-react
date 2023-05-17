6307db302aab9311ed6fd68c5594da4ef65cd44d
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 15:21:47 -0300

    fix: adequando as opções de países recebidas da API para colocá-las num Select optioons

--- src/components/SelectorArea.jsx
+++ src/components/SelectorArea.jsx
@@ -2,18 +2,22 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 
 function SelectorArea(props) {
-  const { text } = props;
+  const { dataArray, text } = props;
 
-  const tests = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
+  //   const tests = [
+  //     { value: 'chocolate', label: 'Chocolate' },
+  //     { value: 'strawberry', label: 'Strawberry' },
+  //     { value: 'vanilla', label: 'Vanilla' },
+  //   ];
+
+  const mountOptions = () => dataArray.map((item) => (
+    { value: item.name, label: item.name }
+  ));
 
   return (
     <>
       <h2>{ text }</h2>
-      <Select options={ tests } />
+      <Select options={ mountOptions() } />
     </>
   );
 }
@@ -21,6 +25,8 @@ function SelectorArea(props) {
 export default SelectorArea;
 
 SelectorArea.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  dataArray: PropTypes.array.isRequired,
   text: PropTypes.string.isRequired,
 };
 
