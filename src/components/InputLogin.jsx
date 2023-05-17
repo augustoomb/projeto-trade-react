ddf7900b839377c05e6a15c5b6c67ae3e02db7c5
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 13:57:10 -0300

    feat: componente input da pagina de login

---
+++ src/components/InputLogin.jsx
@@ -0,0 +1,21 @@
+// import PropTypes from 'prop-types';
+
+// function InputLogin(props) {
+//   const { placeholder, token, setToken } = props;
+
+//   return (
+//     <input
+//       placeholder={ placeholder }
+//       type="text"
+
+//     />
+//   );
+// }
+
+// export default InputLogin;
+
+// InputLogin.propTypes = {
+//   placeholder: PropTypes.string.isRequired,
+//   token: PropTypes.string.isRequired,
+//   setToken: PropTypes.func.isRequired,
+// };
