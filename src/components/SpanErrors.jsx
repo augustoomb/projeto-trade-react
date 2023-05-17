9f471a72806e04d34c7cb073188db8f6d08eb496
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 13:57:26 -0300

    feat: componente span de erros

---
+++ src/components/SpanErrors.jsx
@@ -0,0 +1,24 @@
+// import PropTypes from 'prop-types';
+
+function SpanErrors() {
+//   const { text } = props;
+
+  return (
+    // <div>
+    //   {
+    //     errors !== undefined ? (
+    //       <span>error</span>
+    //     ) : (
+    //       null
+    //     )
+    //   }
+    // </div>
+    <span>Error</span>
+  );
+}
+
+export default SpanErrors;
+
+// SpanErrors.propTypes = {
+//   text: PropTypes.string.isRequired,
+// };
