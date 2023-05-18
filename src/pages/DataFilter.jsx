811584cda269341fdd0a8954b4b1f66934b7d750
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 22:24:23 -0300

    feat: chamando todos os componentes de filtro na pagina DataFilter

--- src/pages/DataFilter.jsx
+++ src/pages/DataFilter.jsx
@@ -1,13 +1,26 @@
 import { useState, useEffect } from 'react';
-import SelectorArea from '../components/SelectorArea';
+import SelectorCountryArea from '../components/SelectorCountryArea';
 import Title from '../components/Title';
 import { reqApiSports } from '../services/apiSports';
 import getTokenInLocalStorage from '../utils/localStorage';
+import SelectorSeasonArea from '../components/SelectorSeasonArea';
+import SelectorLeagueArea from '../components/SelectorLeagueArea';
+import SelectorTeamArea from '../components/SelectorTeamArea';
 
 function DataFilter() {
   const [countries, setCountries] = useState([]);
+  const [seasons, setSeasons] = useState([]);
+  const [leagues, setLeagues] = useState([]);
+  const [teams, setTeams] = useState([]);
+
+  const [selectedCountryName, setSelectedCountryName] = useState('');
+  const [selectedSeasonYear, setSelectedSeasonYear] = useState('');
+  const [selectedLeagueId, setSelectedLeagueId] = useState('');
 
   const COUNTRY_ENDPOINT = 'countries';
+  const SEASON_ENDPOINT = 'leagues/seasons';
+  const LEAGUES_ENDPOINT = 'leagues';
+  const TEAMS_ENDPOINT = 'teams';
 
   const requestCountries = async () => {
     const token = getTokenInLocalStorage();
@@ -15,23 +28,84 @@ function DataFilter() {
     if (dataCountries.data?.results) {
       setCountries(dataCountries.data.response);
     }
-    // setMyBooks(dataBooks);
+  };
+
+  const requestSeasons = async () => {
+    const token = getTokenInLocalStorage();
+    const dataSeasons = await reqApiSports(SEASON_ENDPOINT, JSON.parse(token));
+    if (dataSeasons.data?.results) {
+      setSeasons(dataSeasons.data.response);
+    }
+  };
+
+  const requestLeagues = async () => {
+    const token = getTokenInLocalStorage();
+    const dataLeagues = await reqApiSports(
+      LEAGUES_ENDPOINT,
+      JSON.parse(token),
+      selectedCountryName,
+    );
+    if (dataLeagues.data?.results) {
+      setLeagues(dataLeagues.data.response);
+    }
+  };
+
+  const requestTeams = async () => {
+    const token = getTokenInLocalStorage();
+    const dataTeams = await reqApiSports(
+      TEAMS_ENDPOINT,
+      JSON.parse(token),
+      { selectedLeagueId,
+        selectedSeasonYear },
+    );
+    console.log(dataTeams);
+    if (dataTeams.data?.results) {
+      setTeams(dataTeams.data.response);
+      // console.log(dataTeams.data.response);
+    }
   };
 
   useEffect(() => {
+    if (selectedCountryName !== '') {
+      console.log('entrou');
+      requestLeagues();
+    }
+  }, [selectedCountryName]);
+
+  useEffect(() => {
+    if (selectedLeagueId !== '') {
+      console.log('entrou2');
+      requestTeams();
+    }
+  }, [selectedLeagueId, selectedSeasonYear]);
+
+  useEffect(() => {
     requestCountries();
+    requestSeasons();
   }, []);
 
   return (
     <div>
       <Title text="Tela de filtros" />
-      {
-        countries.length > 0 ? (
-          <SelectorArea dataArray={ countries } text="Escolha um país" />
-        ) : (
-          null
-        )
-      }
+      <SelectorCountryArea
+        dataArray={ countries }
+        text="Escolha um país"
+        setSelectedCountryName={ setSelectedCountryName }
+      />
+      <SelectorSeasonArea
+        dataArray={ seasons }
+        text="Escolha uma temporada"
+        setSelectedSeasonYear={ setSelectedSeasonYear }
+      />
+      <SelectorLeagueArea
+        dataArray={ leagues }
+        text="Escolha uma liga"
+        setSelectedLeagueId={ setSelectedLeagueId }
+      />
+      <SelectorTeamArea
+        dataArray={ teams }
+        text="Escolha um time"
+      />
     </div>
   );
 }
