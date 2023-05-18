d50997458d971fd7bc41006ce34979465a38b13d
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 22:23:53 -0300

    feat: criando componente individual para a área de select do filtro por times

---
+++ src/components/SelectorTeamArea.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+
+function SelectorTeamArea(props) {
+  const { dataArray, text } = props;
+
+  const mountOptions = () => dataArray.map((item) => (
+    { value: item.team.id, label: item.team.name }
+  ));
+
+  // const changeSeasonYear = (event) => {
+  //   setSelectedSeasonYear(event.value);
+  // };
+
+  return (
+    <>
+      <h2>{ text }</h2>
+      <Select
+        isDisabled={ dataArray.length === 0 }
+        options={ mountOptions() }
+        // onChange={ changeSeasonYear }
+      />
+    </>
+  );
+}
+
+export default SelectorTeamArea;
+
+SelectorTeamArea.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  dataArray: PropTypes.array.isRequired,
+  text: PropTypes.string.isRequired,
+  // setSelectedSeasonYear: PropTypes.func.isRequired,
+};
+
+// Fonte: https://react-select.com/home
