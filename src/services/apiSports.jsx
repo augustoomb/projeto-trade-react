b9c918f3d0caa6d7be76c5df09524b7f8f7a3741
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-17 22:21:37 -0300

    fix: método que faz requisição para API agora diferencia requisições por tipo de parâmetro

--- src/services/apiSports.jsx
+++ src/services/apiSports.jsx
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-const reqApiSports = async (endpoint, token) => {
+const reqApiSports = async (endpoint, token, param = false) => {
+  const url = () => {
+    if (!param) {
+      return `https://v3.football.api-sports.io/${endpoint}`;
+    }
+    if (typeof param === 'string') {
+      return `https://v3.football.api-sports.io/${endpoint}?country=${param}`;
+    }
+    return `https://v3.football.api-sports.io/${endpoint}?league=${param.selectedLeagueId}&season=${param.selectedSeasonYear}`;
+  };
   try {
     const response = await axios({
       method: 'GET',
       mode: 'no-cors',
-      url: `https://v3.football.api-sports.io/${endpoint}`,
+      url: url(),
       headers: {
         'x-rapidapi-key': token,
         'x-rapidapi-host': 'v3.football.api-sports.io',
