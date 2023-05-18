d67f184fd49d9d96c132a742fce8fc42f93ee228
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-18 09:54:05 -0300

    feat: página que vai receber o id do time (buscando do estado - redux)

--- src/pages/TeamData.jsx
+++ src/pages/TeamData.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function TeamData() {
+  const selector = useSelector(({ teamId }) => (teamId));
+
   return (
-    <div>TeamData</div>
+    <div>{ selector }</div>
   );
 }
 
