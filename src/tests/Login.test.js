83e44b88e9f9503d82ae2807a67a4b707350c96c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-05-19 15:03:47 -0300

    test: criação de teste para página de login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -11,4 +11,16 @@ describe('Teste da tela de Login', () => {
     const title = screen.getByRole('heading', { name: /React/i, level: 1 });
     expect(title).toBeInTheDocument();
   });
+  test('se há um input para digitar a key / token', () => {
+    renderWithRouter(<Login />);
+
+    const input = screen.getByPlaceholderText(/Digite sua key/i);
+    expect(input).toBeInTheDocument();
+  });
+  test('se há um botão de login', () => {
+    renderWithRouter(<Login />);
+
+    const buttonLogin = screen.getByRole('button', { name: /Login/i });
+    expect(buttonLogin).toBeInTheDocument();
+  });
 });
